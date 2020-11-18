222258f9c7df633fa7284ed3ddd8046e23649a0c
Youri Cadet <cadet.jesusyouri@gmail.com>
2020-11-17 19:46:09 -0500

    commit tree 004862f77c6542c8372f64fb1116285e20eb9915

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(until_midnight)
+    while until_midnight > 0 
+        puts "#{until_midnight} SECOND(S)!"
+        until_midnight -=1
+    end 
+    "HAPPY NEW YEAR!"
+
 end
+
+
+def countdown_with_sleep(sleepy_midnight)
+    while sleepy_midnight > 0
+        puts "#{sleepy_midnight} SECOND(S)!"
+        sleep(1)
+        sleepy_midnight -=1
+    end 
+    "HAPPY NEW YEAR"
+end 
